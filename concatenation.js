73fa240d4026a4416daf59fbae6a2c6514afacf5
auromita93 <nagcha@uw.edu>
2017-04-20 07:22:16 -0400

    added concatenation.js

---
+++ concatenation.js
@@ -0,0 +1,32 @@
+var numString = [];
+$(function enterValue() {
+   
+    $('#numStringForm').on('submit', enterValue(){
+    numString.push($('#numStringForm :textarea').val());
+    console.log(JSON.stringify(numString));
+    
+    //sum of array
+    var sum = 0;
+    for (var i = 0; i <numString.length; i++){
+        sum = sum + numString[i];
+    } 
+    
+    if ($.isInteger($'#numStringForm :textarea').val()) {
+        $('#count').text(numString.length);
+        $('#sum').text(sum.toFixed());
+        $('#mean').text(sum/count.toFixed());
+    }
+    else if (typeof ('#numStringForm :textarea') === 'string'){
+        $('#words').text(numString.length);
+        $('#concatenate').text(numString.join(' '));
+    };
+
+$('#reset').click(function resetForm(){
+    location.reload();
+                  
+ });
+}
+    
+ 
+
+
